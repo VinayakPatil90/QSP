18d863ab293b606d977a3e71ed721742ad37cf3e
VinayakPatil90 <vpinak97@gmail.com>
2026-03-12 16:23:16 +0530

    added new files

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,37 +1,32 @@
 import { test, expect } from '@playwright/test';
+import { beforeEach } from 'node:test';
 
-test('has logo', async ({ page }) => {
-  await page.goto('https://demoapps.qspiders.com/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page.locator('#qspiderLogo')).toBeVisible();
+test.beforeEach('has logo', async ({ page }) => {
+  await page.goto('https://demoapps.qspiders.com/ui?scenario=1');
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://demoapps.qspiders.com/ui?scenario=1');
-
   // Click the get started link.
   await page.getByPlaceholder('Enter your name').fill('Vinayak');
   await page.getByPlaceholder('Enter Your Email').fill('Test@gamil.com')
   await page.locator('#password').pressSequentially('Test123')
-  await page.getByRole('button',{name:'Register'}).click()
+  await page.getByRole('button', { name: 'Register' }).click()
 
   await expect(page.getByRole('status').first()).toHaveText('Registered successfully')
 });
-test('Mouse Action',async({page})=>{
-    await page.goto('https://demoapps.qspiders.com/ui?scenario=1')
-    await page.getByText('Mouse Actions').click()
-});
-test('frames',async({page})=>{
-    await page.goto('https://demoapps.qspiders.com/ui?scenario=1')
-    await page.getByText('Frames').click()
-});
-test('pop-ups',async({page})=>{
-    await page.goto('https://demoapps.qspiders.com/ui?scenario=1')
-    await page.getByText('Popups').click()
+
+test('Mouse Action', async ({ page }) => {
+  await page.getByText('Mouse Actions').click()
 });
 
-test('Date & Time Picker',async({page})=>{
-    await page.goto('https://demoapps.qspiders.com/ui?scenario=1')
-    await page.getByText('Date & Time Picker').click()
+test('frames', async ({ page }) => {
+  await page.getByText('Frames').click()
+});
+
+test('pop-ups', async ({ page }) => {
+  await page.getByText('Popups').click()
+});
+
+test('Date & Time Picker', async ({ page }) => {
+  await page.getByText('Date & Time Picker').click()
 })
