d86f197b3fcd67065706818eb9603946bcebf00a
VinayakPatil90 <vpinak97@gmail.com>
2026-03-12 18:23:58 +0530

    test

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -3,4 +3,5 @@ import { test, expect } from '@playwright/test';
 
 test('Navigate to application', async ({ page }) => {
   await page.goto('https://demoapps.qspiders.com/ui?scenario=1');
+  await page.getByRole('button')
 });
