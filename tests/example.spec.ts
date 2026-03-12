b537000fd8d0d4ec304b115f205281a1370fe4a2
VinayakPatil90 <vpinak97@gmail.com>
2026-03-12 15:48:35 +0530

    updated code

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,21 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('has logo', async ({ page }) => {
+  await page.goto('https://demoapps.qspiders.com/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page.locator('#qspiderLogo')).toBeVisible();
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://demoapps.qspiders.com/ui?scenario=1');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByPlaceholder('Enter your name').fill('Vinayak');
+  await page.getByPlaceholder('Enter Your Email').fill('Test@gamil.com')
+  await page.locator('#password').pressSequentially('Test123')
+  await page.getByRole('button',{name:'Register'}).click()
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(page.getByRole('status').first()).toHaveText('Registered successfully')
+
 });
