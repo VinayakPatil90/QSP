8746be7989cb69141f5b14dc3241ba6832ba5770
VinayakPatil90 <vpinak97@gmail.com>
2026-03-15 19:18:32 +0530

    updated yaml and added tests in test2.spec.ts

--- tests/test2.spec.ts
+++ tests/test2.spec.ts
@@ -28,13 +28,30 @@ test('Table', async ({ page }) => {
     expect(cells).toEqual(data)
 
     //All data from one column
-    const allPrice =  await page.locator('tbody tr td:nth-child(5)').allTextContents()
-    for(let price of allPrice){
+    const allPrice = await page.locator('tbody tr td:nth-child(5)').allTextContents()
+    for (let price of allPrice) {
         console.log(price)
     }
 });
 
 test('Dynamic Table', async ({ page }) => {
-  await page.getByText('Web Table').click();
-  await page.getByText('Dynamic Web Table').click();
+    await page.getByText('Web Table').click();
+    await page.getByText('Dynamic Web Table').click();
+    const row = await page.getByRole('row', { name: "Samsung Galaxy" })
+    await row.locator('td').nth(4).locator('.cursor-pointer').click()
+
+    const form = await page.locator('form aside')
+    await form.locator('select[name="quantity"]').click()
+    await page.locator('form aside select[name="quantity"]').selectOption('2');
+    await form.locator('#updatebtn').click()
 });
+
+test('Table Sort', async ({ page }) => {
+    await page.getByText('Web Table').click();
+    await page.getByText('Dynamic Web Table').click();
+    const header = await page.locator('thead tr')
+    const quantityColumn = await header.getByText("Quantity")
+    await quantityColumn.click()
+    const quantities = await page.locator('tbody tr td:nth-child(3)').allTextContents()
+    expect(quantities).toEqual(["2", "3", "5", "7"])
+})
